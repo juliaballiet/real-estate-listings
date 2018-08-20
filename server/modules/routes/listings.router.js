4d8695d0f900258e9d14b763f2376f920cdb1f83
Julia Balliet <julia.m.balliet@gmail.com>
2018-08-20 16:32:36 -0500

    added pool.js file

--- server/modules/routes/listings.router.js
+++ server/modules/routes/listings.router.js
@@ -1,24 +1,7 @@
 // requires
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-
-// sql
-const Pool = pg.Pool;
-const config = {
-    database: 'real_estate',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 10000
-}
-const pool = new Pool(config);
-pool.on('connect', () => {
-    console.log('postgresql connected');
-});
-pool.on('error', (error) => {
-    console.log('error: ', error);
-});
+const pool = require('../pool');
 
 // routes
 router.post('/', (req, res) => {
